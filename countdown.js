892f46f6b569eaed9a65232b9acef0259858769e
Aaron Watkins <watkins.aaron2@gmail.com>
2022-10-03 17:22:40 +0100

    Updating date after tests

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 11, 2022 14:00:00").getTime();
+var countDownDate = new Date("Dec 11, 2022 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "QIAN IS BACK!!";
   }
 }, 1000);
