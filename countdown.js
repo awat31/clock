1cb10d1c7d367b84c9a33bed9ea02c514351b7eb
Aaron Watkins <watkins.aaron2@gmail.com>
2022-10-03 18:18:04 +0100

    countdown based on prxomity to due date not set values

--- countdown.js
+++ countdown.js
@@ -1,13 +1,21 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 11, 2022 18:00:00").getTime();
+var countDownDate = new Date("Dec 11, 2022 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
 
   // Get today's date and time
   var now = new Date().getTime();
+
+//**DEV ENTRY-------------------------------------------------
+  //var now = 1670781540000
+//**END DEV ENTRIES------------------------------------------
+
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
+
+//**DEV ENTRIES--------------------------------------------------------
+  //document.getElementById("distance").innerHTML = distance
 
   //var rightnow = now;
   //document.getElementById("now").innerHTML = now
@@ -15,37 +23,34 @@ var x = setInterval(function() {
   //var duedate = countDownDate;
   //document.getElementById("duedate").innerHTML = countDownDate
 
+//**END DEV----------------------------------------------------------
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+  // Display the result in the element with id="countdown"
   document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
 // If time is passed 10/12/2022 18:00:00, (within 24 hours) then only print hours
-  if (now > 1670695200000) {
+  if (distance < 86400000) {
 
-    document.getElementById("countdown").innerHTML = hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById("countdown").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
   }
 
-// If time is passed 11/12/2022 17:00:00, (within 1 hour) then only print hours
-  if (now > 1670778000000) {
+// If time is passed 11/12/2022 17:00:00, (within 1 hour) then only print minutes
+  if (distance < 3600000) {
 
     document.getElementById("countdown").innerHTML = minutes + "m " + seconds + "s ";
   }
 
-  if (now > 1670781540000) {
-
+// If time is passed 11/12/2022 17:59:00, (within 1 minute) then only print seconds
+  if (distance < 60000) {
     document.getElementById("countdown").innerHTML = seconds + "s ";
   }
-
-  // Display the result in the element with id="demo"
-
-
-
 
 // If the count down is finished, write some text
   if (distance < 0) {
