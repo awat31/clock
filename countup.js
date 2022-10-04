8490e526d4ab88a386900125ccb0b598893d59ac
Aaron Watkins <49282648+awat31@users.noreply.github.com>
2022-10-04 12:00:29 +0100

    Update countup.js
    11pm 04/10/2022

--- countup.js
+++ countup.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countUpDate = new Date("Oct 04, 2022 10:00:00").getTime();
+var countUpDate = new Date("Oct 04, 2022 23:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
